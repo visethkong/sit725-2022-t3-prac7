846d4bafd52423b01dd4e578e996ad96e54000f4
viseth <52366670+visethkong@users.noreply.github.com>
2022-12-13 22:58:51 +1100

    Practical Week 6

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.use(express.urlencoded({extends: false}));
 app.use(cors())
 app.use('/api/projects', projectRoutes)
 
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next)
+{
+    var firstNumber = parseInt(req.params.firstNumber)
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+        res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+
+})
+
 // //mongoDb connection...
 // const MongoClient = require('mongodb').MongoClient;
 

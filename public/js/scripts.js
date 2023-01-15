2feea78182f6cdd5b2a515a5485ccf23c2b4cd9b
viseth <52366670+visethkong@users.noreply.github.com>
2023-01-15 19:28:32 +1100

    updated for practical 7

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,9 @@
+// connect to the socket
+let socket = io();
+socket.on('number', (msg) => {
+    console.log('Random number: ' + msg);
+})
+
 const getProjects = () => {
     $.get('/api/projects',(response) => {
         if(response.statusCode==200){
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ let projectCollection;
 let dbConnect = require("./dbConnect");
 let projectRoutes = require("./routes/projectRoutes")
 
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({extends: false}));
@@ -24,6 +27,18 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next)
     else { res.json({result: result, statusCode: 200}).status(200) } 
 
 })
+
+
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+    socket.on('disconnect', ()=> {
+        console.log('user disconnected');
+    });
+    setInterval(()=>{
+        socket.emit('number', parseInt(Math.random()*10));
+    }, 1000);
+});
+
 
 // //mongoDb connection...
 // const MongoClient = require('mongodb').MongoClient;
@@ -95,8 +110,7 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next)
 // })
 
 var port = process.env.port || 3000;
-
-app.listen(port, ()=>{
+http.listen(port, ()=>{
     console.log("App listening to: http://localhost:"+port)
     // createCollection("Pets")
 })
